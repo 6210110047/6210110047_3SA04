3ca009ab79d0fa1ab753032323ef9cecf2e14183
Uekiz <kingghostdragon@gmail.com>
2021-07-12 10:08:53 +0700

    forecast

--- components/Weather.js
+++ components/Weather.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native'
+import Forecast from './Forecast';
+
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        })
     return (
-    <Text>{props.zipCode}</Text>
+    <Forecast {...forecastInfo}/>
     );
 }
