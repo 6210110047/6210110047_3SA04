0c04fd88d87a35687b6b8137c95bfc0a9da0c76a
Uekiz <kingghostdragon@gmail.com>
2021-07-18 20:36:08 +0700

    fetch province - zip from api

--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
 import { FlatList, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
-const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
-]
+
 const ZipItem = ({ place, code, navigation }) => (
     <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
-        <View style = {styles.zipItem}>
+        <View style={styles.zipItem}>
             <Text>{place}</Text>
             <Text >{code}</Text>
         </View>
@@ -21,6 +15,38 @@ const ZipItem = ({ place, code, navigation }) => (
 const _keyExtractor = item => item.code
 
 export default function ZipCodeScreen() {
+
+    const [availableZipItems] = useState([
+
+    ])
+
+    useEffect(() => {
+        console.log(`fetching data`)
+
+        fetch(`https://raw.githubusercontent.com/rathpanyowat/Thai-zip-code-latitude-and-longitude/master/data.json`)
+            .then((response) => response.json())
+            .then((json) => {
+                for (let instace of json) {
+                    var check = true
+                    for (let checkdup of availableZipItems) {
+                        if (instace["province"] == checkdup.place) {
+                            check = false
+                            break
+                        }
+                    }
+                    if (check == true) {
+                        availableZipItems.push({
+                            place: instace["province"],
+                            code: instace["zip"]
+                        });
+                    }
+                }
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+    })
+
     const navigation = useNavigation()
     return (
         <View>
