6351b0d0acedd22a548ae965f604b0b98b238d5f
Uekiz <kingghostdragon@gmail.com>
2021-07-18 19:32:50 +0700

    route navigation

--- App.js
+++ App.js
@@ -3,23 +3,37 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import Weather from './components/Weather'
+import WeatherScreen from './components/WeatherScreen'
+import ZipCodeScreen from './components/ZipCodeScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-
+const Stack = createStackNavigator();
 export default function App() {
-  const doIt = () => {
-    console.log("Hello from console")
-  }
   return (
-    
-    <View style={styles.container}>
-      <Weather zipCode="90110" />
-      <StatusBar style="auto" />
-    </View>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight
-  },
-});
+// export default function App() {
+//   const doIt = () => {
+//     console.log("Hello from console")
+//   }
+//   return (
+
+//     <View style={styles.container}>
+//       <Weather zipCode="90110" />
+//       <StatusBar style="auto" />
+//     </View>
+
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: Constants.statusBarHeight
+//   },
+// });
